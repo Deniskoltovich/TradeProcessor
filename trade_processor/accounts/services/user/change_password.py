6325e3580680a2a827fe496b79ddb716af5d6c17
Denis Koltovich <koltovich1123@gmail.com>
2023-07-02 14:42:29 +0300

    email notifications on changed password && orders && activation email

--- trade_processor/accounts/services/user/change_password.py
+++ trade_processor/accounts/services/user/change_password.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import logout
 
 from accounts.serializers.user_serializers import PasswordUserSerializer
+from accounts.tasks.send_notification_password_changed import (
+    send_notification_about_changed_password,
+)
 
 
 class UpdateUserPasswordService:
@@ -10,6 +13,7 @@ class UpdateUserPasswordService:
         if serializer.is_valid():
             user.set_password(serializer.validated_data['password'])
             user.save()
+            send_notification_about_changed_password.delay(user.id)
             logout(request)
             return {'status': 'password set'}
         return serializer.errors
--- trade_processor/accounts/tasks/send_notification_password_changed.py
+++ trade_processor/accounts/tasks/send_notification_password_changed.py
@@ -1,28 +1,16 @@
-from celery.utils.log import get_task_logger
+from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 
-from accounts.services.auth_service import AuthService
 from config import celery_app, settings
-
-logger = get_task_logger(__name__)
 
 
 @celery_app.task
-def send_confirmation_email(user_id):
+def send_notification_about_changed_password(user_id):
 
-    """
-    The send_confirmation_email function sends an email to the user
-    with a link that they can click on to activate their account.
-
-    :param user_id: Get the user from the database
-    :return: None
-    """
-    activation_link, user = AuthService.generate_activation_link(user_id)
-
+    user = get_user_model().objects.get(pk=user_id)
     send_mail(
-        subject='Account confirmation',
-        message=f'Confirmation link: {activation_link}',
+        subject='Changing password',
+        message="Your trading bot account's password was changed",
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[user.email],
     )
-    logger.info('Email is send')
