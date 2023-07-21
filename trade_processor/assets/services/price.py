b6f23f2f85d63436357058988977d8aea4afd554
Denis Koltovich <koltovich1123@gmail.com>
2023-07-21 13:25:48 +0300

    minor fixes

--- trade_processor/assets/services/price.py
+++ trade_processor/assets/services/price.py
@@ -6,8 +6,6 @@ from assets.models import Asset
 class PriceService:
     @staticmethod
     def update_price(asset_data: dict):
-        print(asset_data)
-        print(type(asset_data))
         asset = Asset.objects.get(name=asset_data['name'])
         asset.current_price = decimal.Decimal(asset_data["current_price"])
         asset.save()
